7062435b597bf2e01824700f625912792a8d7383
romina <saezr.ro@gmail.com>
2018-11-30 17:24:03 -0300

    actividad-realizada

--- test/app.js
+++ test/app.js
@@ -1,7 +1,48 @@
-$.ajax({
-	url: 'http://www.mindicador.cl/api',
-	method: 'GET'
-}).then(data){
-	console.log(data);
-};
+(function($){
 
+	var currency = {
+		dolar: 0,
+		euro: 0,
+		utm: 0,
+		uf: 0,
+	
+	}
+
+	$.ajax({
+		url: 'http://www.mindicador.cl/api/',
+		method: 'GET'
+	}).done(function(data){
+		console.log(data)
+
+		currency.dolar = data.dolar.valor
+		currency.euro  = data.euro.valor
+		currency.uf    = data.uf.valor
+		currency.utm   = data.utm.valor	
+
+		$('.idolar').html('$'+ currency.dolar)
+		$('.ieuro').html('$'+ currency.euro)
+		$('.iuf').html('$'+ currency.uf)
+		$('.iutm').html('$' + currency.utm)
+		
+		
+	});
+
+	$('.convertir').on('submit', function(event) {
+		event.preventDefault();
+		var valor_campo = parseFloat($('.moneda').val())
+
+		$('._dolar').text((valor_campo / currency.dolar).toFixed(2))		
+		$('._euro').text((valor_campo / currency.euro).toFixed(2))
+		$('._uf').text((valor_campo / currency.uf).toFixed(2))
+		$('._utm').text((valor_campo / currency.utm).toFixed(2))		
+
+		// console.log(valor_campo)
+	});
+
+	$('.transformar').on('submit', function(event) {
+		event.preventDefault();
+		var valor_dolar = parseInt($('.cash').val())
+
+		$('._dolarpeso').text((currency.dolar * valor_dolar).toFixed(0))
+});
+})(jQuery)
